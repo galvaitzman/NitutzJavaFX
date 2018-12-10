7436e9a2f08837213b9c1769341949af8bed0fca
gvaitzma <galvaitzman@gmail.com>
2018-12-11 00:37:12 +0200

    create vacation controller,fxml,view

--- src/main/java/Controller/UpdateVacationDetailsController.java
+++ src/main/java/Controller/UpdateVacationDetailsController.java
@@ -28,7 +28,14 @@ public class UpdateVacationDetailsController extends Controller {
         public void handle(Event event) {
                 if (!updateVacationDetailsView.isValidDate()) return;
                 else if (!updateVacationDetailsView.isAllFieldsFull()) return;
-
+                String[] updateVacation = vacationModel.getCurrentVacation().getVacation_details();
+                vacationModel.updateVacation(Integer.parseInt(updateVacation[0]), updateVacation[2], updateVacation[3], updateVacation[4],
+                            updateVacation[5], updateVacation[6], updateVacation[7], updateVacation[8], updateVacation[9],
+                            updateVacation[10], updateVacation[11], updateVacation[12], updateVacation[13], Integer.parseInt(updateVacation[14]),
+                            updateVacation[15], updateVacation[16], updateVacation[17], updateVacation[18], updateVacation[19],
+                            updateVacation[20], updateVacation[21], updateVacation[22], updateVacation[23],updateVacation[24],
+                            updateVacation[25], updateVacation[26], updateVacation[27], updateVacation[28]);
+                updateVacationDetailsView.alert("Updated successfully");
                 window.close();
         }
     }
