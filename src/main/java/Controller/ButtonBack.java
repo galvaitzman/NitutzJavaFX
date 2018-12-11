221befac67fd8035e1ad786120f378c16f723030
gvaitzma <galvaitzman@gmail.com>
2018-12-11 18:59:56 +0200

    create vacation controller,fxml,view

---
+++ src/main/java/Controller/ButtonBack.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class ButtonBack implements EventHandler {
+    private static MainController mainController;
+    public static void setMainConroller(MainController other){
+        mainController = other;
+    }
+    @Override
+    public void handle(Event event) {
+        mainController.closeWindow();//
+        mainController.goBackToPreviousController();
+    }
+}
